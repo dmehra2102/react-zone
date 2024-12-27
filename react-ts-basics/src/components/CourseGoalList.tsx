2a51dd8035352dd58fe8171c3bb874b5139b6b21
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-12-27 21:49:33 +0530

    feat: added advance component setup and done with infobox componenet

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as CourseGoalType } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type Props = {
   goals: CourseGoalType[];
@@ -7,15 +9,32 @@ type Props = {
 };
 
 export default function CourseGoalList({ goals, onDeleteGoal }: Props) {
+  if (!goals.length) {
+    return (
+      <InfoBox mode="hint">You have no goals yet. Start adding some!</InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="medium">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
