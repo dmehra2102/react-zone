d9ce7810c3c3e5c5e340b58e944ed916e8afe006
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-12-28 01:34:23 +0530

    feat : added container polymorphic component

--- advance-components/src/App.tsx
+++ advance-components/src/App.tsx
@@ -1,4 +1,5 @@
 import Button from "./components/Button";
+import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <Button href="http://youtube.com" target="_blank">
         My Link
       </Button>
+
+      <Container as={"button"}>?</Container>
     </main>
   );
 }
